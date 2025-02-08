991283c6c2d1db42c6136a11297b387b229c1212
miguel977 <miguel645@gmail.com>
2025-02-08 08:03:15 -0300

    criação do projeto django2

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    # context = {
+    #     'produtos': Produto.objects.all()
+    # }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def produto(request):
+    return render(request, 'produto.html')
+
