b93aada74ac63f0a2b35e893482ded335270d1d6
miguel977 <miguel645@gmail.com>
2025-02-21 09:23:11 -0300

    Salvando dados no banco de dados

--- core/views.py
+++ core/views.py
@@ -35,12 +35,8 @@ def produto(request):
         if str(request.method) == 'POST':
             form = ProdutoModelForm(request.POST, request.FILES)
             if form.is_valid():
-                prod = form.save(commit = False)
                 
-                print(f'nome{prod.nome}')
-                print(f'preço{prod.preco}')
-                print(f'Estoque{prod.estoque}')
-                print(f'Imagem{prod.imagem}')
+                form.save()
 
                 messages.success(request, 'Produto salvo com sucesso.')
                 form = ProdutoModelForm()
@@ -52,3 +48,5 @@ def produto(request):
             'form': form
         }
         return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
